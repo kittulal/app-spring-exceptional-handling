83a2d936d7aba8fab807a4430e5e27eed61d6cfa
kittulal <punnakrishnaa@gmail.com>
2026-01-14 09:29:18 +0530

    first commit

---
+++ src/main/java/com/krish/exceptional_handling/AppSpringExceptionalHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.krish.exceptional_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringExceptionalHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringExceptionalHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krish/exceptional_handling/AppSpringExceptionalHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krish.exceptional_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringExceptionalHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
